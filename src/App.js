8797dcc39e7a7b36318815f40684c27c32a0fe86
conqueror1988 <fatih.aksoy1988@gmail.com>
2023-12-01 10:51:27 +0100

    finished

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import DataGridforNBATeams from "./components/DataGrid";
+
+function App() {
+  return (
+    <div className="App">
+      <DataGridforNBATeams />
+    </div>
+  );
+}
+
+export default App;
